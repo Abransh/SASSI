d8f9c73767f08a89efcce3fc8783b64068f85dfb
abransh <abranshbaliyan2807@gmail.com>
2025-03-04 16:51:05 +0100

    making Social Icons

--- sassiweb/components/Footer.tsx
+++ sassiweb/components/Footer.tsx
@@ -1,4 +1,4 @@
-/ components/Footer.tsx
+//components/Footer.tsx
 import Link from 'next/link';
 import SocialIcons from './SocialIcons';
 
@@ -89,3 +89,25 @@ const Footer = () => {
                   Politecnico di Milano, Milan, Italy
                 </span>
               </li>
+              <li className="flex items-start">
+                <svg className="h-6 w-6 mr-2 text-indigo-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+                </svg>
+                <span className="text-gray-400">
+                  info@isapolimi.org
+                </span>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        {/* Copyright */}
+        <div className="border-t border-gray-800 mt-12 pt-8 text-center text-gray-400">
+          <p>&copy; {currentYear} ISA POLIMI. All rights reserved.</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
