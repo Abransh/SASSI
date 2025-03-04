0f54a4343f9c9ef7cede39dd1c11954290f03f78
abransh <abranshbaliyan2807@gmail.com>
2025-03-04 15:57:46 +0100

    making get involved

---
+++ sassiweb/components/GetInvolved.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link"
+
+export default function GetInvolvedSection() {
+  return (
+    <section id="about" className="py-20 relative overflow-hidden">
+      <div className="container mx-auto px-4">
+        <div className="relative">
+          {/* Image container - replace with actual image later */}
+          <div className="w-full h-[500px] bg-gray-200 rounded-lg overflow-hidden">
+            {/* Placeholder for image */}
+            <div className="w-full h-full flex items-center justify-center text-gray-400">
+              Image will be placed here
+            </div>
+          </div>
+
+          {/* Get Involved Button */}
+          <div className="absolute left-10 bottom-10 md:left-20 md:bottom-20"></div>
