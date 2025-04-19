9f048cca3b7f4195e60448d6c7dfcf3d33c77bed
abransh <abranshbaliyan2807@gmail.com>
2025-04-19 23:57:38 +0200

    updating maps

--- sassiweb/next.config.js
+++ sassiweb/next.config.js
@@ -42,12 +42,12 @@ const nextConfig = {
             key: 'Content-Security-Policy',
             value: `
               default-src 'self' https://ucarecdn.com https://uploadcare.com https://js.stripe.com https://api.stripe.com https://checkout.stripe.com;
-              script-src 'self' 'unsafe-inline' 'unsafe-eval' https://ucarecdn.com https://uploadcare.com https://js.stripe.com;
+              script-src 'self' 'unsafe-inline' 'unsafe-eval' https://ucarecdn.com https://uploadcare.com https://js.stripe.com https://maps.googleapis.com;
               style-src 'self' 'unsafe-inline' https://ucarecdn.com https://uploadcare.com https://checkout.stripe.com;
-              img-src 'self' blob: data: https://ucarecdn.com https://sassimilan.com https://uploadcare.com https://*.stripe.com;
+              img-src 'self' blob: data: https://ucarecdn.com https://sassimilan.com https://uploadcare.com https://*.stripe.com https://maps.gstatic.com https://maps.googleapis.com;
               media-src 'self' blob: https://ucarecdn.com https://uploadcare.com;
-              connect-src 'self' https://ucarecdn.com https://uploadcare.com https://api.uploadcare.com https://api.stripe.com;
-              frame-src 'self' https://ucarecdn.com https://uploadcare.com https://js.stripe.com https://hooks.stripe.com https://checkout.stripe.com;
+              connect-src 'self' https://ucarecdn.com https://uploadcare.com https://api.uploadcare.com https://api.stripe.com https://maps.googleapis.com;
+              frame-src 'self' https://ucarecdn.com https://uploadcare.com https://js.stripe.com https://hooks.stripe.com https://checkout.stripe.com https://www.google.com;
               font-src 'self' data:;
               object-src 'none';
               base-uri 'self';
