160c9f2b7ab1a3f1b804ec2c4a790ca78c11ed31
abransh <abranshbaliyan2807@gmail.com>
2025-05-11 20:42:51 +0200

    updating how to upload images

--- sassiweb/components/EventDetail.tsx
+++ sassiweb/components/EventDetail.tsx
@@ -174,7 +174,7 @@ export default function EventDetail({ event }: EventDetailProps) {
               {/* Event Status Badge */}
               <div className="relative">
                 {event.imageUrl ? (
-                  <div className="relative w-full" style={{ aspectRatio: '306 / 100' }}>
+                  <div className="relative w-full" style={{ aspectRatio: '351 / 100' }}>
                     <Image
                       src={event.imageUrl}
                       alt={event.title}
