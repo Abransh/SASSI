d3bcba18174ae599d0af7bdec6b4958f600e3166
abransh <abranshbaliyan2807@gmail.com>
2025-05-12 02:24:48 +0200

    updated image size on mobile and desktop

--- sassiweb/components/EventDetail.tsx
+++ sassiweb/components/EventDetail.tsx
@@ -174,7 +174,7 @@ export default function EventDetail({ event }: EventDetailProps) {
               {/* Event Status Badge */}
               <div className="relative">
                 {event.imageUrl ? (
-                  <div className="relative w-full md:aspect-[7/3] aspect-[7/5]">
+                  <div className="relative w-full md:aspect-[7/3] aspect-[7/4]">
                     <Image
                       src={event.imageUrl}
                       alt={event.title}
