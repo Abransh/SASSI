9646fdf36d0827549582396daef41d3eefe6743e
abransh <abranshbaliyan2807@gmail.com>
2025-06-10 16:12:55 +0200

    updated email

--- sassiweb/lib/email.ts
+++ sassiweb/lib/email.ts
@@ -97,7 +97,7 @@ We look forward to working with you and celebrating the spirit of yoga together!
                 <a href="https://www.sassimilan.com" 
              style="background-color: #9999ff; color: white; padding: 10px 20px; text-decoration: none; border-radius: 5px; display: inline-block;">
             Our Website
-          </a>  <br><a href=" https://www.instagram.com/sassi.milan/<" 
+          </a>  <br><a href=" https://www.instagram.com/sassi.milan/" 
              style="background-color: ##ff3300; color: white; padding: 10px 20px; text-decoration: none; border-radius: 5px; display: inline-block;">
            Follow us on Instagram
           </a>  /p>
